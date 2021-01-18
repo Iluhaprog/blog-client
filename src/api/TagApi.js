7a5f7fbc3031f5bf336865359bba4b275384beb0
Ilya Novak <rickmortyand4@gmail.com>
2021-01-18 18:45:07 +0300

    Writed DAL for tag

---
+++ src/api/TagApi.js
@@ -0,0 +1,30 @@
+import api from './api';
+
+function create(tag) {
+    return api.post('/tag/create', { tag });
+}
+
+function getById(id) {
+    return api.get('/tag/getById', {
+        params: { id },
+    });
+}
+
+function getByPostId(postId) {
+    return api.get('/tag/getByPostId', {
+        params: { postId },
+    });
+}
+
+function deleteById(id) {
+    return api.delete('/tag/deleteById', {
+        params: { id },
+    });
+}
+
+export default {
+    create,
+    getById,
+    getByPostId,
+    deleteById,
+};
