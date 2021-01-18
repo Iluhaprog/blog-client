1bd7397d92be4622ca02366a358bd4dfebf4ca99
Ilya Novak <rickmortyand4@gmail.com>
2021-01-18 19:37:47 +0300

    Writed DAL for post

---
+++ src/api/PostApi.js
@@ -0,0 +1,48 @@
+import api from './api';
+
+function getById(id) {
+    return api.get('/post/getById', {
+        params: { id },
+    });
+}
+
+function getByUserId(userId) {
+    return api.get('/post/getByUserId', {
+        params: { userId },
+    });
+}
+
+function getAll(page, limit) {
+    return api.get(`/post/getAll/${page}/${limit}`);
+}
+
+function create(post) {
+    return api.post('/post/create', { post });
+}
+
+function setTags(postId, tagsId) {
+    return api.put('/post/setTags', {
+        postId,
+        tagsId,
+    });
+}
+
+function update(post) {
+    return api.put('/post/update', { post });
+}
+
+function deleteById(id) {
+    return api.delete('/post/deleteById', {
+        params: { id },
+    });
+}
+
+export default {
+    create,
+    getById,
+    getAll,
+    getByUserId,
+    setTags,
+    update,
+    deleteById,
+};
