8e7119ead1882d059210451ac503e641c08b5e0c
Ilya Novak <rickmortyand4@gmail.com>
2021-01-19 17:03:30 +0300

    Writed DAL for like

---
+++ src/api/LikeApi.js
@@ -0,0 +1,43 @@
+import api from './api';
+
+function getById(id) {
+    return api.get('/like/getById', {
+        params: { id },
+    });
+}
+
+
+function getAll() {
+    return api.get('/like/getAll');
+}
+
+function getByUserId(userId) {
+    return api.get('/like/getByUserId', {
+        params: { userId },
+    });
+}
+
+function getByPostId(postId) {
+    return api.get('/like/getByPostId', {
+        params: { postId },
+    });
+}
+
+function create(like) {
+    return api.post('/like/create', { like });
+}
+
+function deleteById(id) {
+    return api.delete('/like/deleteById', {
+        params: { id },
+    });
+}
+
+export default { 
+    getById,
+    getByPostId,
+    getByUserId,
+    getAll,
+    create,
+    deleteById,
+};
