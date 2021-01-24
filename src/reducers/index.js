a52b151fe0538754830a87dbf827af5af2729ae1
Ilya Novak <rickmortyand4@gmail.com>
2021-01-24 15:14:27 +0300

    Changed imports

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { reduser as formReduser } from 'redux-form';
-import userReduser from './UserReduser';
-import postReduser from './PostReduser';
+import userReduser from './UserReducer';
+import postReduser from './PostReducer';
 import projectReducer from './ProjectReducer';
 
 export default combineReducers({
