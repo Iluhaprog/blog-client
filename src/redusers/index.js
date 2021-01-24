0a415f3c0fc0791078a6c374a44dbc5640c75c4a
Ilya Novak <rickmortyand4@gmail.com>
2021-01-24 15:06:31 +0300

    Added project reduser to index.js

--- src/redusers/index.js
+++ src/redusers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reduser as formReduser } from 'redux-form';
 import userReduser from './UserReduser';
 import postReduser from './PostReduser';
+import projectReducer from './ProjectReducer';
 
 export default combineReducers({
     user: userReduser,
     post: postReduser,
+    project: projectReducer,
     form: formReduser,
 });
