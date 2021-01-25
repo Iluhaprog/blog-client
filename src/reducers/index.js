b2ef77c6d237de090278c0fcf03579754e90448d
Ilya Novak <rickmortyand4@gmail.com>
2021-01-25 11:27:12 +0300

    Added tag reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { reduser as formReduser } from 'redux-form';
 import userReduser from './UserReducer';
 import postReduser from './PostReducer';
 import projectReducer from './ProjectReducer';
+import tagReducer from './TagReducer';
 
 export default combineReducers({
     user: userReduser,
     post: postReduser,
     project: projectReducer,
+    tags: tagReducer,
     form: formReduser,
 });
