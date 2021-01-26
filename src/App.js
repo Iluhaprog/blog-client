15c5a262cf56a1d87481b193cf6c2b086beb5dad
Ilya Novak <rickmortyand4@gmail.com>
2021-01-26 11:16:59 +0300

    Added basic styles for app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import './App.scss';
+import Nav from './components/nav/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="App__box App__box_left">
+        <Nav />
+      </div>
+      <main className="App__box">
+
+      </main>
     </div>
   );
 }
