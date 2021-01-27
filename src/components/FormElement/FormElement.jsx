310b31311b66a19d1bdc25cead0e04ce991098c5
Ilya Novak <rickmortyand4@gmail.com>
2021-01-27 16:35:55 +0300

    Added form element

---
+++ src/components/FormElement/FormElement.jsx
@@ -0,0 +1,13 @@
+import { Field } from "redux-form";
+import './formElement.scss';
+
+export default props => (
+    <div className='form__box row-reverse'>
+        <Field 
+            type={props.type}
+            name={props.name}
+            component={props.component}
+        />
+        <label htmlFor={props.name}>{props.label}</label>
+    </div>
+);
