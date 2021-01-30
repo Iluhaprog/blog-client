4735fcbfce1e78a206ca65bcf499b5f19ecda0e1
Ilya Novak <rickmortyand4@gmail.com>
2021-01-30 13:11:20 +0300

    Added test for labeled input

---
+++ src/components/formElements/LabeledInput/labledInput.test.js
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { reduxForm } from 'redux-form';
+import { Provider } from 'react-redux';
+import LabeledInput from './LabeledInput';
+import configureMockStore from 'redux-mock-store';
+
+const mockStore = configureMockStore();
+
+
+const decor = el => {
+    const form = () => (<form>{el}</form>);
+    return reduxForm({
+        form: 'test',
+    })(form);
+}
+
+test('Test labled input view', () => {
+    const store  = mockStore({});
+    const Lbin = decor(
+        <LabeledInput 
+            type='text'
+            name='test'
+            component='input'
+            label='TEST'
+        />
+    );
+    const { asFragment } = render(<Provider store={store}><Lbin /></Provider>);
+    expect(asFragment()).toMatchSnapshot();
+});
