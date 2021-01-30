e6e281d08ec2aff6c73122aef3497ee2b1b5c7a7
Ilya Novak <rickmortyand4@gmail.com>
2021-01-30 12:38:50 +0300

    Added onClick prop

--- src/components/buttons/SendButton/SendButton.jsx
+++ src/components/buttons/SendButton/SendButton.jsx
@@ -1,7 +1,7 @@
 import './sendButton.scss';
 
 export default props => (
-    <button className='button send-button'>
+    <button className='button send-button' onClick={props.onClick}>
         {props.text}
     </button>
 );
