22c8ca274bc173ec5b099a18e3cbfcb8809552d5
Ilya Novak <rickmortyand4@gmail.com>
2021-01-30 14:59:49 +0300

    Added redirect to admin page by condition

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,13 +2,16 @@ import { connect } from 'react-redux';
 import { LoginForm } from '../components/forms';
 import { loginFetch } from '../actoins/user';
 import { Center } from '../components/containers';
+import { Redirect } from 'react-router-dom';
 
 const Login = props => {
-    const { status, errorData } = props;
+    const { status, errorData, authorized } = props;
 
     const submit = values => {
         props.login(values.username, values.password);
     };
+
+    if (authorized) return <Redirect to='/admin/profile' />
 
     return (
         <Center>
@@ -19,6 +22,7 @@ const Login = props => {
 
 const mapStateToProps = state => ({
     status: state.user.status,
+    authorized: state.user.authorized,
     errorData: state.user.errorData,
 });
 
