e5f74db61a004672176540e60a02798b6e1558a1
Ilya Novak <rickmortyand4@gmail.com>
2021-02-01 14:12:43 +0300

    Added the corresponding exports and imports

--- src/components/boxes/index.js
+++ src/components/boxes/index.js
@@ -1,5 +1,9 @@
 import FormBox from './FormBox';
+import { RoundBox } from './RoundBox';
+import { HintBox } from './HintBox';
 
 export {
     FormBox,
+    RoundBox,
+    HintBox,
 };
--- src/components/formElements/index.js
+++ src/components/formElements/index.js
@@ -1,5 +1,9 @@
 import LabeledInput from './LabeledInput';
+import { Textarea } from './Textarea';
+import { DropZone } from './DropZone';
 
 export {
     LabeledInput,
+    Textarea,
+    DropZone,
 };
