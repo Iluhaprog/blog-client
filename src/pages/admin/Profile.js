969fc50c59a7aaefc76d893d271f65c4c2ca72a4
Ilya Novak <rickmortyand4@gmail.com>
2021-02-01 18:08:44 +0300

    Added ProfileForm on Profile page

--- src/pages/admin/Profile.js
+++ src/pages/admin/Profile.js
@@ -1,5 +1,34 @@
-export default props => (
-    <div>
-        Profile page
-    </div>
-);
+import { connect } from "react-redux";
+import { updateAvatarFetch, updateFetch } from "../../actoins/user";
+import { ProfileForm } from "../../components/forms/ProfileForm";
+
+const Profile = props => {
+    const submit = values => {
+        const { avatar } = values;
+        const user = {...values};
+        delete user['avatar'];
+        
+        if (avatar !== props.avatar) {
+            const formData = new FormData();
+            formData.append('avatar', avatar);
+            props.updateAvatar(formData);
+        }
+        props.update(user);
+    }
+    return (
+        <div className='admin-page'>
+            <ProfileForm onSubmit={submit}/>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    avatar: state.user.avatarImage,
+});
+
+const mapDispatchToProps = dispatch => ({
+    update: user => dispatch(updateFetch(user)),
+    updateAvatar: avatar => dispatch(updateAvatarFetch(avatar)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
