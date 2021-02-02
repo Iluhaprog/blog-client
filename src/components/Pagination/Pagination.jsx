d7f825087f27173f2577cafb4ff02f68f2803447
Ilya Novak <rickmortyand4@gmail.com>
2021-02-02 20:48:49 +0300

    Added pagination component with test

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react"
+import { NavLink } from "react-router-dom";
+import { PrimaryButton } from "../buttons";
+import { Row } from '../containers';
+import './pagination.scss';
+
+function Pagination({ totalItems, itemsPerPage, currentPage, changePage, visiblePageLinks = 3 }) {
+    const visiblePageLinksIndexInit = Math.floor((currentPage - 1) / visiblePageLinks);
+    const [visiblePageLinksIndex, setVisiblePageLinksIndex] = useState(visiblePageLinksIndexInit);
+
+    const totalPagesLength = Math.ceil(totalItems / itemsPerPage);
+    const visiblePageLinksIndexTotal = Math.ceil(totalPagesLength / visiblePageLinks);
+
+    const totalPages = totalPagesLength < visiblePageLinks 
+            ? totalPagesLength 
+            : visiblePageLinksIndex + 1 === visiblePageLinksIndexTotal 
+                ? visiblePageLinks - Math.abs(visiblePageLinks * visiblePageLinksIndexTotal - totalPagesLength)
+                : visiblePageLinks;
+    const pages = Array(totalPages).fill(0);
+    return (
+        <div className='pagination'>
+            <Row justifyContent='c' alignItems='c'>
+                {
+                    visiblePageLinksIndex 
+                        ? (
+                            <PrimaryButton 
+                                onClick={() => setVisiblePageLinksIndex(visiblePageLinksIndex - 1)}
+                                text='<'/>
+                        ) 
+                        : ''
+                }
+                {
+                    pages.map((_, index) => (
+                        <NavLink 
+                            key={index} 
+                            to={`/admin/posts/${index + 1 + visiblePageLinksIndex * visiblePageLinks}`}
+                            onClick={() => changePage(index + visiblePageLinksIndex * visiblePageLinks)}
+                            className={
+                                index + 1 + visiblePageLinksIndex * visiblePageLinks === currentPage 
+                                    ? 'selected-page'
+                                    : '' 
+                            }
+                        >
+                            {index + 1 + visiblePageLinksIndex * visiblePageLinks}
+                        </NavLink>
+                    ))
+                }
+                {
+                    (totalPagesLength > visiblePageLinks && (visiblePageLinksIndex + 1) * visiblePageLinks < totalPagesLength ) && (
+                        <PrimaryButton 
+                            onClick={() => setVisiblePageLinksIndex(visiblePageLinksIndex + 1)}
+                            text='>'
+                        />
+                    )
+                }
+            </Row>
+        </div>
+    )
+}
+
+export default Pagination;
