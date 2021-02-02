8f85e83601988dfc493a38592295ae8afabea657
Ilya Novak <rickmortyand4@gmail.com>
2021-02-02 12:44:53 +0300

    Added PostCard component with tests

---
+++ src/components/PostCard/PostCard.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Row } from '../containers';
+import { TagBox } from '../boxes';
+import wave1 from '../../assets/post_card/wave1.png';
+import wave2 from '../../assets/post_card/wave2.png';
+import './postCard.scss';
+import { DangerButton, PrimaryButton } from '../buttons';
+
+export default props => {
+
+    return (
+        <article className='post-card'>
+            <div className="wave-box">
+                <div className="wave-box__bg">
+                    { props.img && <img src={props.img} alt="post-preview" /> }
+                </div>
+                <div className="wave-box__effect">
+                    <div className="wave-box__item">
+                        <img src={wave1} alt="wave1" />
+                    </div>
+                    <div className="wave-box__item">
+                        <img src={wave2} alt="wave2" />
+                    </div>
+                    <div className="wave-box__item">
+                        <img src={wave1} alt="wave3" />
+                    </div>
+                    <div className="wave-box__item">
+                        <img src={wave2} alt="wave4" />
+                    </div>
+                </div>
+            </div>
+            <div className="post-description">
+                <div className="column">
+                    <h3 className="post-description__title">{props.title}</h3>
+                    <p className="post-description__text">
+                        {props.description}
+                    </p>
+                    <ul className='tags'>
+                        <Row justifyContent='fs' alignItems='c' wrap='w'>
+                            { 
+                                props.tags && props.tags.map(tag => (
+                                    <TagBox key={tag.id} title={tag.title} />
+                                ))
+                            }
+                        </Row>
+                    </ul>
+                    <div className="post-card__bottom">
+                        <Row justifyContent='sb' alignItems='c' wrap='w'>
+                            <Row>
+                                <PrimaryButton text='Change' onClick={props.onClick} />
+                                <DangerButton text='Delete' onClick={props.onDelete} />
+                            </Row>
+                            <time className="date">
+                                {props.date}
+                            </time>
+                        </Row>
+                    </div>
+                </div>
+            </div>
+        </article>
+    );
+}
---
+++ src/components/PostCard/postCard.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import PostCard from './PostCard';
+
+describe('Test post card view', () => {
+    test('Should view post card', () => {
+        const { asFragment } = render(
+            <PostCard 
+                title='Test title'
+                description='test description'
+                date='25.04.2020'
+                tags={[
+                    {id: 1, title: 'React'}, 
+                    {id: 2, title: 'JS'}, 
+                    {id: 3, title: 'NODEJS'}
+                ]}
+            />
+        );
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe('Test post card events', () => {
+    test('Should handle onclick', async () => {
+        const handleClick = jest.fn();
+        const { container } = render(
+            <PostCard onClick={handleClick} />
+        );
+        fireEvent.click(container.querySelector('.primary-button'));
+        await waitFor(() => {
+            expect(handleClick).toHaveBeenCalledTimes(1);
+        });
+    });
+    test('Should handle delete', async () => {
+        const handleDelete = jest.fn();
+        const { container } = render(
+            <PostCard onDelete={handleDelete} />
+        );
+        fireEvent.click(container.querySelector('.danger-button'));
+        await waitFor(() => {
+            expect(handleDelete).toHaveBeenCalledTimes(1);
+        });
+    });
+});
