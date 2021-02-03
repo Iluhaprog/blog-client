05c3d3cf04dfe455fdf4c90e970c620269d23905
Ilya Novak <rickmortyand4@gmail.com>
2021-02-03 15:30:37 +0300

    Added component for modal window

---
+++ src/components/modals/Modal/Modal.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { DangerButton, SuccessButton } from '../../buttons';
+import { Row } from '../../containers';
+import './modal.scss';
+
+function Modal(props) {
+    const { modal, close, setFail} = props;
+    const { text, handleSuccess, fail, withInput, visible } = modal;
+    const [value, setValue] = useState('');
+
+    return (
+        <div className={`modal ${visible ? 'modal_visible' : ''}`}>
+            <Row justifyContent='c' alignItems='c'>
+                <div className='modal-window'>
+                    <p className='modal-window__text'>
+                        {text}
+                    </p>
+                    {
+                        withInput 
+                            ? (
+                                <input 
+                                    type='text' 
+                                    className='modal-window__input'
+                                    onChange={e => setValue(e.target.value)}
+                                    value={value}
+                                />
+                              )
+                            : ''
+                    }
+                    {
+                        fail.message 
+                            ? (<strong>{fail.message}</strong>)
+                            : '' 
+                    }
+                    <Row justifyContent='sb'>
+                        <SuccessButton 
+                            text='ok'
+                            onClick={() => {
+                                handleSuccess(value, setFail);
+                            }}
+                        />
+                        <DangerButton 
+                            text='close' 
+                            onClick={() => {
+                                close();
+                                setValue('');
+                            }}
+                        />
+                    </Row>
+                </div>
+            </Row>
+        </div>
+    );
+}
+
+export default Modal;
