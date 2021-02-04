40ff55e2715b9516b4358f9866ef9cc95886311c
Ilya Novak <rickmortyand4@gmail.com>
2021-02-04 17:54:03 +0300

    Added post form

---
+++ src/components/forms/PostForm/PostForm.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { reduxForm, Field, change, arrayPush, getFormValues } from 'redux-form';
+import { SendButton, SuccessButton } from '../../buttons';
+import { Row } from '../../containers';
+import { 
+    LabeledInput, 
+    MarkdownRedactor, 
+    DropZone, 
+    MultipleSelect,
+    Textarea
+} from '../../formElements';
+import { addTagFetch, getAllFetch } from '../../../actoins/tag';
+import { setModal } from '../../../actoins/modal'
+import './postForm.scss';
+
+function PostForm(props) {
+    const { handleSubmit, openModalWindow, setSelect } = props;
+
+    useEffect(() => {
+        props.getAllTags();
+    }, [props.tags])
+
+    return (
+        <form onSubmit={handleSubmit} className='post-form'>
+            <LabeledInput 
+                type='text'
+                name='title'
+                component='input'
+                label='Title'
+            />
+            <Row>
+                <Field
+                    name='preview'
+                    component={DropZone}
+                />
+                <Textarea 
+                    name='description'
+                    placeholder='Description'
+                />
+            </Row>
+            <MarkdownRedactor 
+                name='text'
+                placeholder='Text'
+                value={props.formValues ? props.formValues.text : ''}
+            />
+            <Row alignItems='fs'>
+                <div className='post-form__tags'>
+                    <p>Tags:</p>
+                    <SuccessButton 
+                        text='New tag'
+                        onClick={e => {
+                            e.preventDefault();
+                            openModalWindow();
+                        }}
+                    />
+                </div>
+                <MultipleSelect
+                    values={props.selected.Tags || []}
+                    options={props.tags}
+                    onChange={option => setSelect(option)}
+                />
+            </Row>
+            <SendButton 
+                text='Send'
+            />
+        </form>
+    );
+}
+
+const mapStateToProps = state => ({
+    initialValues: {
+        ...state.post.selected,
+    },
+    selected: state.post.selected,
+    tags: state.tags.tags,
+    formValues: getFormValues('postForm')(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+    openModalWindow: () => {
+        dispatch(setModal({
+            text: 'Write tag title:',
+            visible: true,
+            withInput: true,
+            handleSuccess: (title, fail) => {
+                dispatch(addTagFetch({ title }))
+            }
+        }));
+    },
+    addTag: tag => {
+        dispatch(addTagFetch(tag));
+    },
+    getAllTags: () => {
+        dispatch(getAllFetch())
+    },
+    setSelect: value => {
+        dispatch(arrayPush('postForm', 'Tags', value))
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({
+    form: 'postForm',
+    enableReinitialize: true,
+})(PostForm));
