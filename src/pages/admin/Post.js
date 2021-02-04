b4c5f28ef27915fe47cdef38939b3fc20c2bf9b3
Ilya Novak <rickmortyand4@gmail.com>
2021-02-04 17:56:20 +0300

    Added PostForm to Page

--- src/pages/admin/Post.js
+++ src/pages/admin/Post.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { 
+    createFileFetch, 
+    selectPostById, 
+    setTagsFetch,
+    updateFetch
+} from '../../actoins/post';
+import { PostForm } from '../../components/forms';
 
-export default props => {
+const Post = props => {
     const { id } = useParams();
+
+    if (!props.selected.id) {
+        props.getPostById(id);
+    }
+
+    const submit = values => {
+        const tags = values.Tags || [];
+        const { title, description, preview, text, visible } = values;
+        if (tags.length) {
+            props.setTags(id, tags);
+            console.log(tags);
+        }
+        props.updatePreview(preview);
+        props.updatePost({
+            id,
+            title,
+            description,
+            text,
+            visible,
+        });
+    }
+
     return (
         <section className='admin-page'>
-            Post id: {id}
+            <PostForm onSubmit={submit}/>
         </section>
     );
 }
+
+const mapStateToProps = state => ({
+    selected: state.post.selected,
+});
+
+const mapDispatchToProps = dispatch => ({
+    getPostById: id => {
+        selectPostById(id);
+    },
+    setTags: (postId, tags) => {
+        dispatch(setTagsFetch(postId, tags || []));
+    },
+    updatePreview: file => {
+        const fd = new FormData();
+        fd.append('preview', file);
+        const dir = process.env.REACT_APP_PREVIEWS_DIR;
+        dispatch(createFileFetch(dir, fd));
+    },
+    updatePost: post => {
+        console.log(post);
+        dispatch(updateFetch(post));
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
