9128d67a68a5f8c44793321dee771f0c17778af9
Ilya Novak <rickmortyand4@gmail.com>
2021-02-05 20:03:27 +0300

    Added post page

--- src/pages/admin/Post.js
+++ src/pages/admin/Post.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { 
+    createFileFetch,
+    getFilesFetch,
     selectPostById, 
     setTagsFetch,
     updateFetch,
     updatePreviewFetch
 } from '../../actoins/post';
-import { PostForm } from '../../components/forms';
+import { ImageLoaderForm, PostForm } from '../../components/forms';
+import { Row } from '../../components/containers';
+import FilesViewer from '../../components/FilesViewer/FilesViewer';
+import { getUniqueName } from '../../util/string/string'
 
 const Post = props => {
     const { id } = useParams();
+    const { dirname, files, getFiles } = props;
 
     if (!props.selected.id) {
         props.getPostById(id);
     }
+
+    useEffect(() => {
+        if (props.selected.directoryId) {
+            getFiles(props.selected.directoryId)
+        }
+    }, [files])
 
     const submit = values => {
         const tags = values.Tags || [];
         const { title, description, preview, text, visible } = values;
         if (tags.length) {
             props.setTags(id, tags);
-            console.log(tags);
         }
-        props.updatePreview(id, preview);
+        if (preview) {
+            props.updatePreview(id,  new File([preview], getUniqueName(preview.name)));
+        }
         props.updatePost({
             id,
             title,
@@ -33,15 +46,29 @@ const Post = props => {
         });
     }
 
+    const sendFile = values => {
+        if (dirname) {
+            props.upload(dirname, values.file);
+        }
+    }
+
     return (
         <section className='admin-page'>
-            <PostForm onSubmit={submit}/>
+            <Row>
+                <PostForm onSubmit={submit}/>
+                <div className='column column_ai-c' style={{width: '40%'}}>
+                    <ImageLoaderForm onSubmit={sendFile}/>
+                    <FilesViewer files={files}/>
+                </div>
+            </Row>
         </section>
     );
 }
 
 const mapStateToProps = state => ({
     selected: state.post.selected,
+    dirname: state.post.dir ? state.post.dir.name : '',
+    files: state.post.files,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -57,9 +84,19 @@ const mapDispatchToProps = dispatch => ({
         dispatch(updatePreviewFetch(postId, fd));
     },
     updatePost: post => {
-        console.log(post);
         dispatch(updateFetch(post));
     },
+    upload: (dirname, file) => {
+        if (file) {
+            const fd = new FormData();
+            fd.append('file', new File([file], getUniqueName(file.name)));
+            console.log(dirname);
+            dispatch(createFileFetch(dirname, fd));
+        }
+    },
+    getFiles: dirId => {
+        dispatch(getFilesFetch(dirId));
+    }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Post)
