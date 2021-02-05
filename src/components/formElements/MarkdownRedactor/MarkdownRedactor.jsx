affce73e44bbdc0312ef0844c2e68e76f6d88a30
Ilya Novak <rickmortyand4@gmail.com>
2021-02-05 15:48:40 +0300

    Changed impor for marked

--- src/components/formElements/MarkdownRedactor/MarkdownRedactor.jsx
+++ src/components/formElements/MarkdownRedactor/MarkdownRedactor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import marked from 'marked';
+import marked from '../../../util/setting/marked';
 import { Textarea } from '../Textarea';
 import './markdownRedactor.scss';
 import { SuccessButton, DangerButton } from '../../buttons';
