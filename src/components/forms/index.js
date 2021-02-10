b18b7abf288155fcac52fc294ffe0569b3c64e6b
Ilya Novak <rickmortyand4@gmail.com>
2021-02-10 21:07:36 +0300

    Added SettingsForm to index js

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -2,10 +2,12 @@ import LoginForm from './LoginForm/LoginForm';
 import { PostForm } from './PostForm';
 import { ImageLoaderForm } from './ImageLoaderForm';
 import { ProjectForm } from './ProjectForm';
+import { SettingsForm } from './SettingsForm';
 
 export {
     LoginForm,
     PostForm,
     ImageLoaderForm,
     ProjectForm,
+    SettingsForm,
 };
