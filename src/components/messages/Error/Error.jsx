6faf458e5922642fe113e93e04f8628fdec378c4
Ilya Novak <rickmortyand4@gmail.com>
2021-02-14 12:36:33 +0300

    Added close button and fix styles

--- src/components/messages/Error/Error.jsx
+++ src/components/messages/Error/Error.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Row } from '../../containers';
+import { CloseButton } from '../../buttons';
 import './error.scss';
 
 export default props => (
     <div className='error-box'>
-        <p className='error-box__message'>
-            {props.message}
-        </p>
+        <Row>
+            <p className='error-box__message'>
+                {props.message}
+            </p>
+            <CloseButton 
+                text='X'
+                onClick={props.onClose}
+            />
+        </Row>
     </div>
 );
