d9143c985406f06561ad0e0b3e8ead9bfdf2bcdc
Ilya Novak <rickmortyand4@gmail.com>
2021-02-17 11:02:14 +0300

    Added mobile version for MainNav

--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { PrimaryButton } from '../buttons';
@@ -8,6 +8,7 @@ import './mainNav.scss';
 
 function MainNav(props) {
     const history = useHistory();
+    const [menuIsVisible, setMenuVisible] = useState(false);
     const links = [
         {path: '/home', name: 'Home' },
         {path: '/search', name: 'Search'},
@@ -16,12 +17,24 @@ function MainNav(props) {
     return (
         <div className='main-nav'>
             <Row justifyContent='sb' alignItems='c'>
-                <nav>
+                <div 
+                    className={`burger-button ${menuIsVisible ? 'burger-button_visible' : ''}`}
+                    onClick={() => setMenuVisible(!menuIsVisible)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <nav className={menuIsVisible ? 'visible' : ''}>
                     <ul className='row'>
                         {
                             links.map(link => (
                                 <li key={link.path}>
-                                    <NavLink to={link.path} activeClassName='selected'>
+                                    <NavLink 
+                                        to={link.path} 
+                                        activeClassName='selected'
+                                        onClick={() => setMenuVisible(!menuIsVisible)}
+                                    >
                                         {link.name}
                                     </NavLink>
                                 </li>
