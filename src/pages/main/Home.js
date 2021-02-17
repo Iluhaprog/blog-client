27c47a9541eb1018c492226b521f999742ce7dce
Ilya Novak <rickmortyand4@gmail.com>
2021-02-17 20:25:50 +0300

    Added ImageBox to Home page

--- src/pages/main/Home.js
+++ src/pages/main/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { ImageBox } from '../../components/boxes/ImageBox';
 
 function Home(props) {
+    const files = process.env.REACT_APP_FILES_URL;
     return (
         <section className='main-page'>
-            Home
+            <ImageBox img={`${files}${props.homePreview}`}>
+                <h1 className='main-page__title'>
+                    {props.homeTitle}
+                </h1>
+            </ImageBox>
         </section>
     );
 }
 
-export default Home;
+const mapStateToProps = state => ({
+    homeTitle: state.home.title,
+    homePreview: state.home.preview,
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
