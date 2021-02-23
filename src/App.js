1225da8bdb406c36ddf2e6f2ab3a5e514509b212
Ilya Novak <rickmortyand4@gmail.com>
2021-02-23 22:03:13 +0300

    Added React lazy and Suspese for Main, Admin pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Admin from './pages/admin';
-import Main from './pages/main';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
 import NotFound from './pages/NotFound';
@@ -8,21 +7,26 @@ import { Modal } from './components/modals/Modal';
 import { Message } from './components/Message';
 import './App.scss';
 
+const Admin = React.lazy(() => import('./pages/admin'));
+const Main = React.lazy(() => import('./pages/main'));
+
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path='/admin' component={Admin} />
-          <Route path='/login' component={Login} />
-          <Route path='/registration' component={Registration} />
-          <Route path='/' component={Main} />
-          <Route component={NotFound} />
-        </Switch>
-        <Modal />
-        <Message />
-      </div>
-    </BrowserRouter>
+    <Suspense fallback={'Loading...'}>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path='/admin' component={Admin} />
+            <Route path='/login' component={Login} />
+            <Route path='/registration' component={Registration} />
+            <Route path='/' component={Main} />
+            <Route component={NotFound} />
+          </Switch>
+          <Modal />
+          <Message />
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
