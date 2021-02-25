1a1b40b0c10301cf0a6274961bbf19cc74f158de
Ilya Novak <rickmortyand4@gmail.com>
2021-02-25 21:30:21 +0300

    Changed UploadLoader component for using basic Loader

--- src/components/loaders/UploadLoader/UploadLoader.jsx
+++ src/components/loaders/UploadLoader/UploadLoader.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import loader from '../../../assets/loader.svg';
+import { Loader } from '../Loader';
 import './uploadLoader.scss';
 
 function UploadLoader(props){
     return (
-        <>
+        <Loader visible={props.visible}>
             <img 
                 src={loader}
                 width={props.width || 30}
-                height={props.height || 30} 
-                className={`loader ${props.visible ? 'loader_visible' : ''}`}
+                height={props.height || 30}
                 alt='loading...'     
             />
-        </>
+        </Loader>
     );
 }
 
