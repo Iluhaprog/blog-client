a9f806538c78d34a483d0b3aba21ca1ae2e3bf96
Ilya Novak <rickmortyand4@gmail.com>
2021-02-25 21:24:30 +0300

    Added basic Loader component

---
+++ src/components/loaders/Loader/Loader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './loader.scss';
+
+function Loader(props) {
+    const isVisible = props.visible ? 'loader_visible' : '';
+    return (
+        <div className={`loader ${isVisible}`}>
+            {props.children}
+        </div>
+    );
+}
+
+Loader.defaultProps = {
+    visible: false,
+};
+
+Loader.propTypes = {
+    visible: PropTypes.bool,
+    children: PropTypes.node,
+};
+
+export default Loader;
