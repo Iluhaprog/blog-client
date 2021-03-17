72472910bc69708f3a7bff8bf6bec85987eaaa62
Ilya Novak <rickmortyand4@gmail.com>
2021-03-17 21:41:56 +0300

    Added decorator for getting token from localstorage

---
+++ src/utils/decorators/decorators.js
@@ -0,0 +1,9 @@
+export function requestWithToken(func) {
+    const accessTokenKey = process.env.REACT_APP_ACCESS_TOKEN_KEY;
+    const accessToken = localStorage.getItem(accessTokenKey);
+
+    const headers = {
+        Authorization: accessToken ? `Bearer ${accessToken}` : '' ,
+    };
+    return func(headers);
+}
