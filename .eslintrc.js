622809783e39936bd0f8611bedd27e6337cb20ec
Ilya Novak <rickmortyand4@gmail.com>
2021-03-19 09:00:33 +0300

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+    'es2021': true,
+    'jest': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+  },
+};
