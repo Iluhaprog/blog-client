c83a34fe28b405cbe0f4e590e4f9d96bf5ee59ae
Ilya Novak <rickmortyand4@gmail.com>
2021-03-24 21:39:53 +0300

    Fix vertical position for ModalScreen

--- src/components/Modal/Styled/ModalScreen.jsx
+++ src/components/Modal/Styled/ModalScreen.jsx
@@ -11,7 +11,7 @@ const ModalScreen = styled.div`
   padding: 10px;
   border-radius: 10px;
   box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
-  margin-top: calc(50vh - ${({maxHeight}) => maxHeight}px);
+  margin-top: calc(50vh - ${({maxHeight}) => maxHeight/2}px);
   background-color: ${({theme}) => {
     return theme === themes.DARK ? '#343a40' : '#f8f9fa';
   }};
