09d5690aca7b84744af2c6bc79da8bb94db93fc2
Ilya Novak <rickmortyand4@gmail.com>
2021-04-03 20:32:35 +0300

    Added ProjectCardList to Projects page

--- src/pages/admin/Projects.js
+++ src/pages/admin/Projects.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import {ProjectHeader} from '../../components/Project/Header';
+import * as PropTypes from 'prop-types';
+import {ProjectCardList} from '../../components/Project/CardList';
+import {connect} from 'react-redux';
+import {getAll} from '../../store/project/projectActions';
 
-function Projects() {
+let Projects = ({getAll}) => {
+  useEffect(() => {
+    getAll();
+  }, []);
+
   return (
     <section style={{padding: '10px 0'}}>
       <Container>
         <ProjectHeader />
+        <ProjectCardList />
       </Container>
     </section>
   );
-}
+};
 
-Projects.propTypes = {};
+Projects.propTypes = {
+  getAll: PropTypes.func,
+};
+
+const mapStateToProps = (state) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  getAll: () => {
+    dispatch(getAll());
+  },
+});
+
+Projects = connect(mapStateToProps, mapDispatchToProps)(Projects);
 
 export {Projects};
