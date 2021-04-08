f9870505849ca7c4a1ddcc006b8dc2dc130b499f
Ilya Novak <rickmortyand4@gmail.com>
2021-04-08 09:20:18 +0300

    Fix style for Description

--- src/components/Public/UI/Description/Description.jsx
+++ src/components/Public/UI/Description/Description.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Description = styled.p`
   font-family: var(--public-font-2);
   font-size: 16px;
+  overflow-wrap: anywhere;
 `;
 
 export {Description};
