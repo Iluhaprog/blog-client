49d69c8a5420f7406172cf51716af222c1492121
Ilya Novak <rickmortyand4@gmail.com>
2021-04-08 14:11:48 +0300

    Added ProjectList component

---
+++ src/components/Public/Project/List/ProjectList.jsx
@@ -0,0 +1,56 @@
+import React, {useEffect} from 'react';
+import {PageTitle} from '../../UI/PageTitle';
+import PropTypes from 'prop-types';
+import {getAll} from '../../../../store/project/projectActions';
+import {connect} from 'react-redux';
+import {Row} from '../../Row';
+import {ProjectCard} from '../Card';
+import {Separator} from '../../Separator';
+
+let ProjectList = ({lang, projects, getProjects}) => {
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  return (
+    <>
+      <PageTitle>{lang.nav.PROJECTS}</PageTitle>
+      <Separator indentBottom={50}/>
+      <Row justifyContent={'space-between'} wrap={'wrap'}>
+        {
+          projects?.map((project) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              projectLink={project.projectLink}
+              githubLink={project.githubLink}
+              preview={project.preview}
+            />
+          ))
+        }
+      </Row>
+    </>
+  );
+};
+
+ProjectList.propTypes = {
+  lang: PropTypes.object,
+  projects: PropTypes.array,
+  getProjects: PropTypes.func,
+};
+
+const mapStateToProps = (state) => ({
+  lang: state.settings.lang,
+  projects: state.project.projects,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getProjects: () => {
+    dispatch(getAll());
+  },
+});
+
+ProjectList = connect(mapStateToProps, mapDispatchToProps)(ProjectList);
+
+export {ProjectList};
