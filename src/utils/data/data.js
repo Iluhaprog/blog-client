4e96577dc46c97fae9b7b06213570fc903c91a7c
Ilya Novak <rickmortyand4@gmail.com>
2021-04-21 17:55:07 +0300

    Added checking for arguments in data util

--- src/utils/data/data.js
+++ src/utils/data/data.js
@@ -1,16 +1,19 @@
 export function getEntityDataByLang(entity, lang, field) {
   const data = entity[field];
-  const currentData = data.find((el) => el.locale.name === lang);
-  const tmp = {};
-  Object.keys(entity).forEach((key) => {
-    if (key !== field) {
-      tmp[key] = entity[key];
-    }
-  });
-  return Object.assign({}, {
-    ...tmp,
-    ...currentData,
-  });
+  if (entity && data) {
+    const currentData = data.find((el) => el.locale.name === lang);
+    const tmp = {};
+    Object.keys(entity).forEach((key) => {
+      if (key !== field) {
+        tmp[key] = entity[key];
+      }
+    });
+    return Object.assign({}, {
+      ...tmp,
+      ...currentData,
+    });
+  }
+  return entity;
 }
 
 export function updateEntityByLang({
@@ -20,18 +23,21 @@ export function updateEntityByLang({
   field = '',
   getFields = () => {},
 }) {
-  const updatedData = getFields(data);
-  const updatedEntityData = entity[field].map((data) => {
-    if (data.locale.name === lang) {
-      return {
-        ...data,
-        ...updatedData,
-      };
-    }
-    return data;
-  });
-  return {
-    ...entity,
-    [field]: updatedEntityData,
-  };
+  if (entity && data) {
+    const updatedData = getFields(data);
+    const updatedEntityData = entity[field].map((data) => {
+      if (data.locale.name === lang) {
+        return {
+          ...data,
+          ...updatedData,
+        };
+      }
+      return data;
+    });
+    return {
+      ...entity,
+      [field]: updatedEntityData,
+    };
+  }
+  return entity;
 }
