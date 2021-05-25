93a8d4e609284ba28a16ff8deea3c087c5cf5d1a
Ilya Novak <rickmortyand4@gmail.com>
2021-05-25 19:05:08 +0300

    Added normalize css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store} from '../src/store/store';
+import 'normalize.css';
 
 ReactDOM.render(
     <Provider store={store}>
